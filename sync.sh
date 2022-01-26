14ed8eabb08a9828c8fca83f888412b36c371e9d
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2022-01-26 23:26:29 +0200

    Updated the sync script and readme for dependancies
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- sync.sh
+++ sync.sh
@@ -2,12 +2,13 @@
 
 
 function sync() {
-    rsync -avz --recursive -R --delete --prune-empty-dirs --exclude '.git' --info=progress2 -e 'ssh -p22 -o "StrictHostKeyChecking=no"' /home/krasi/src/github.com/arribada/smart-camera  root@192.168.0.116:/
+    rsync -avz --recursive -R --delete --prune-empty-dirs --exclude '.git' --info=progress2 -e 'ssh -p22 -o "StrictHostKeyChecking=no"' /home/$USER/src/github.com/arribada/smart-camera  root@raspberrypi.local:/
+    rsync -avz --recursive -R --delete --prune-empty-dirs --exclude '.git' --info=progress2 -e 'ssh -p22 -o "StrictHostKeyChecking=no"' /home/$USER/src/github.com/groupgets/pylepton  root@raspberrypi.local:/
 }
 
 sync
 
-while inotifywait -r -e modify,create,delete /home/krasi/src/github.com/arribada/smart-camera
+while inotifywait -r -e modify,create,delete /home/$USER/src/github.com/arribada/smart-camera
 do
     sync
 done 
